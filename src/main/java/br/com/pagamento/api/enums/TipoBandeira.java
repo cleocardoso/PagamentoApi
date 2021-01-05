a9d94c46ebe42b7677262013ee7af93dd642fb20
ProjetoDSW <cleotads21@gmail.com>
2021-01-05 14:27:47 -0300

    primeiro commit

---
+++ src/main/java/br/com/pagamento/api/enums/TipoBandeira.java
@@ -0,0 +1,6 @@
+package br.com.pagamento.api.enums;
+
+public enum TipoBandeira {
+
+	VISA, MASTERCARD, ELO, AMERICAN_EXPRESS, HIPERCARD, DESCONHECIDO;
+}
---
+++ src/main/java/br/com/pagamento/api/service/EmailService.java
@@ -0,0 +1,43 @@
+package br.com.pagamento.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import br.com.pagamento.api.model.Email;
+import br.com.pagamento.api.model.User;
+
+
+@Service
+public class EmailService { 
+		
+	@Autowired
+	private UserService serviceUsuario;
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	
+	public void sendNovaSenhaEmail(Email email, String novaSenha) {
+		try {
+			User usuario = serviceUsuario.getEmail(email.getTo());
+			email.setFrom("suporte.projeto.siap@gmail.com");
+			email.getMap().put("name", usuario.getNome());
+			email.setSubject("Olá " + usuario.getNome() + " Confira sua nova senha!");
+			
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setTo(email.getTo());
+			message.setSubject(email.getSubject());
+			message.setText("Nova senha: " + novaSenha);
+			message.setFrom(email.getFrom());
+			
+		    mailSender.send(message);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
