df1a349628278412fcd698308eb40e1f9a101f32
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-01-07 15:32:24 -0300

    testando aplicação  no heroku

--- src/main/java/br/com/pagamento/api/PagamentoApiApplication.java
+++ src/main/java/br/com/pagamento/api/PagamentoApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PagamentoApiApplication {
 
+	//testando aplicação
 	public static void main(String[] args) {
 		SpringApplication.run(PagamentoApiApplication.class, args);
 	}
